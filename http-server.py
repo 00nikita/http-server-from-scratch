5eae26c454c113b0315921685b8984a9721f84b1
00nikita <nikhithadubbaka2003@gmail.com>
2026-07-17 23:55:06 +0530

    creating socket

--- http-server.py
+++ http-server.py
@@ -5,6 +5,12 @@ Server_port = 8080
 
 #creating socket
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((Server_host, Server_port))
+server_socket.listen(1)
+print('Listening on port %s ...' % Server_port)
+
 
 
 #binding socket
