f9ef3d7c03d8c791bab1bb6b153490322289482f
00nikita <nikhithadubbaka2003@gmail.com>
2026-07-20 22:13:19 +0530

    accepting multiple requests from single socket

--- http-server.py
+++ http-server.py
@@ -10,13 +10,16 @@ socket.bind((host, port))
 #listen to connections 
 socket.listen()
 #acccept connection
+client_connection = None
 while True:
+    if(client_connection==None):
+        client_connection, client_address = socket.accept()
     #listening socket opens a client socket when a request is received
-    client_connection, client_address = socket.accept()
     client_request = client_connection.recv(1024).decode() # it comes in bytes, after which it is decoded and it turns to http format, which client browser sends accoriding to protocol.
     print(client_request)
-    response = client_connection.sendall(response.encode())
-    client_connection.close()
+    response = 'HTTP/1.0 200 OK\n\nHello World\n'
+    client_connection.sendall(response.encode())
+    # client_connection.close()
 
 #close listening socket
 socket.close()
