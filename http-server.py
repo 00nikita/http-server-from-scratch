ccee457e4166ec290cdcee84847b70b541d8ecd3
00nikita <nikhithadubbaka2003@gmail.com>
2026-07-21 20:00:28 +0530

    printing request and headers (made as dictionary)

--- http-server.py
+++ http-server.py
@@ -14,9 +14,19 @@ while True:
     client_connection, client_address = socket.accept()
     #listening socket opens a client socket when a request is received
     client_request = client_connection.recv(1024).decode() # it comes in bytes, after which it is decoded and it turns to http format, which client browser sends accoriding to protocol.
-    print("method:", client_request.split()[0])
-    print("path:", client_request.split()[1])
-    print("version:", client_request.split()[2])
+    request_line = client_request.splitlines()[0]
+    method, path, version = request_line.split()
+    print("method:", method)    
+    print("path:", path)
+    print("version:", version)
+    lines = client_request.splitlines()
+    headers = {}
+    for line in lines[1:]:
+        if line=="":
+            break
+        key, value = line.split(":", 1)
+        headers[key]=value.strip()
+    print("Headers:", headers) 
     response = 'HTTP/1.0 200 OK\n\nHello World\n'
     client_connection.sendall(response.encode())
     # client_connection.close()
