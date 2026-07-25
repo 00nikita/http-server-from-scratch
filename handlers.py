e42021b74cb7ed9ee04d6993f0cfe9aa4eecc509
00nikita <nikhithadubbaka2003@gmail.com>
2026-07-25 21:36:55 +0530

    dynamic response

--- handlers.py
+++ handlers.py
@@ -1,20 +1,22 @@
 def home():
     with open("pages/index.html") as f:
-        body = f.read()
-    return "HTTP/1.1 200 OK", body
+        page = f.read()
+    return "HTTP/1.1 200 OK", page
 def about():
     with open("pages/about.html") as f:
-        body = f.read()
-    return "HTTP/1.1 200 OK", body
+        page = f.read()
+    return "HTTP/1.1 200 OK", page
 def login_page():
     with open("pages/login.html") as f:
-        body = f.read()
-    return "HTTP/1.1 200 OK", body
+        page = f.read()
+    return "HTTP/1.1 200 OK", page
 def process_login(headers, body):
     with open("pages/login_success.html") as f:
-        body = f.read()
-    return "HTTP/1.1 200 OK", body
+        page = f.read()
+    username = body["username"]
+    page = page.replace("{{username}}", username)
+    return "HTTP/1.1 200 OK", pagec
 def not_found():
     with open("pages/not_found.html") as f:
-        body = f.read()
-    return "HTTP/1.1 404 Not Found", body
+        page = f.read()
+    return "HTTP/1.1 404 Not Found", page
