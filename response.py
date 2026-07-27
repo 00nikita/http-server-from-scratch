6a16b86640f8fd7b5ee24d63355aca638b75d7a0
00nikita <nikhithadubbaka2003@gmail.com>
2026-07-27 22:59:40 +0530

    add Server, Date, Connection to response header

--- response.py
+++ response.py
@@ -1,10 +1,16 @@
+from datetime import datetime, timezone
+
 def response_builder(status, response_headers, body):
-    # if isinstance(body, str):
-    #     body = body.encode()
+    now = datetime.now(timezone.utc)
+    date = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
+
     response = (
         f"{status}\r\n"
         f"Content-Type: {response_headers['Content-Type']}\r\n"
         f"Content-Length: {len(body)}\r\n"
+        f"Server: NikhithaHTTP/1.0\r\n"
+        f"Date: {date}\r\n"
+        f"Connection: close\r\n"
         f"\r\n"
     ).encode()
     return response+body
